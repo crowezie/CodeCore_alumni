acd5ba2216cc8f53c08777800b811561589490c3
Kosuke <kondo1017@gmail.com>
2015-09-19 13:40:38 -0700

    Refacotor 5 support controllers(projects skills assets educations experiences)

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,21 +1,21 @@
 class SkillsController < ApplicationController
-  # These are basic functions. Need to modify when we need.
-
   before_action :authenticate_user!
   before_action :find_profile
-  before_action :find_skill, only: [:show, :edit, :update, :destroy]
+  before_action :find_skill, only: [:edit, :update, :destroy]
+  before_action :authorize!, only: [:edit, :update, :destroy]
 
   def new
     @skill   = Skill.new
   end
 
   def create
-    @skill      = Skill.new skill_params
-    @skill.user = current_user
+    @skill          = Skill.new skill_params
+    @skill.profile  = @profile
+    @skill.user     = current_user
     if @skill.save
-      redirect_to profile_path(@profile), notice: "Skill created!"
+      redirect_to profile_path(@profile), notice: "Skill Created!"
     else
-      flash[:alert] = "See errors below"
+      flash[:alert] = "See Errors Below"
       render :new
     end
   end
@@ -25,15 +25,16 @@ class SkillsController < ApplicationController
 
   def update
     if @skill.update skill_params
-      redirect_to profile_path(@profile), notice: "Skill updated!"
+      redirect_to profile_path(@profile), notice: "Skill Updated!"
     else
+      flash[:alert] = "See Errors Below"
       render :edit
     end
   end
 
   def destroy
     @skill.destroy
-    redirect_to profile_path(@profile), notice: "Skill deleted!"
+    redirect_to profile_path(@profile), notice: "Skill Deleted!"
   end
 
   private
@@ -43,11 +44,16 @@ class SkillsController < ApplicationController
   end
 
   def find_skill
-    @skill = Skill.find(params[:id])
+    @skill   = Skill.find(params[:id])
   end
 
   def skill_params
     params.require(:skill).permit([:name, :level])
+  end
+
+  def authorize!
+    redirect_to root_path, alert: "Access Denied"
+    unless can? :manage, @skill
   end
 
 end
