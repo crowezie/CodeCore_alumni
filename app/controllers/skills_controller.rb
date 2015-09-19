802939db5aab9e565f5d3c49111c26b79f140c38
Kosuke <kondo1017@gmail.com>
2015-09-19 11:45:37 -0700

    Added controller skills(Please pull this one)

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,30 +1,30 @@
 class SkillsController < ApplicationController
   # These are basic functions. Need to modify when we need.
 
+  before_action :authenticate_user!
+  before_action :find_profile
+  before_action :find_skill, only: [:show, :edit, :update, :destroy]
+
   def new
-    @skill = Skill.new
+    @skill   = Skill.new
   end
 
   def create
-    @skill      = Skill.new(skill_params)
+    @skill      = Skill.new skill_params
     @skill.user = current_user
     if @skill.save
-      redirect_to skill_path(@skill), notice: "Skill created!"
+      redirect_to profile_path(@profile), notice: "Skill created!"
     else
       flash[:alert] = "See errors below"
     end
-  end
-
-  def show
-    @skill = Skill.find(params[:id])
   end
 
   def edit
   end
 
   def update
-    if @skill.update(skill_params)
-      redirect_to skill_path(@skill)
+    if @skill.update skill_params
+      redirect_to profile_path(@profile), notice: "Skill updated!"
     else
       render :edit
     end
@@ -32,13 +32,21 @@ class SkillsController < ApplicationController
 
   def destroy
     @skill.destroy
-    redirect_to root_path, notice: "Skill deleted!"
+    redirect_to profile_path(@profile), notice: "Skill deleted!"
   end
 
   private
 
+  def find_profile
+    @profile = Profile.find params[:profile_id]
+  end
+
+  def find_skill
+    @skill = Skill.find(params[:id])
+  end
+
   def skill_params
-    params.require(:skill).permit()
+    params.require(:skill).permit([:name, :level])
   end
 
 end
