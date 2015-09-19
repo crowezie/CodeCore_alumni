7b79f24c39a405f14097a44224c0d681dd1d438c
Kosuke <kondo1017@gmail.com>
2015-09-18 19:29:08 -0700

    Added seed file(Create profile faker)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+100.times do
+  user =
+  User.create(
+    first_name:      Faker::Name.first_name,
+    last_name:       Faker::Name.last_name,
+    email:           Faker::Internet.email,
+    password_digest: BCrypt::Password.create("password")
+  )
+  user.profile = Profile.create(
+    description:  Faker::Lorem.paragraph,
+    availability: true,
+    pitch:        Faker::Lorem.sentences(1),
+    image:        Faker::Avatar.image,
+    phone:        Faker::PhoneNumber.phone_number,
+    email:        Faker::Internet.email,
+    location:     Faker::Address.city
+  )
+end
