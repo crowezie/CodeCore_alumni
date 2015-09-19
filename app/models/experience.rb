d573a25915e7bea7b7d8a1a1ede1a7129cb07266
TABISH KHAN <tabish003@hotmail.com>
2015-09-19 13:35:47 -0700

    fixed typos in project and experience models

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -4,7 +4,7 @@ class Experience < ActiveRecord::Base
 
   validates :position, presence: true
   validates :company, presence: true
-  validates :description, presence: true, length: {maxmum: 500}
+  validates :description, presence: true, length: {maximum: 500}
   validates :weblink, presence: true
   validates :logo
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,9 +2,9 @@ class Project < ActiveRecord::Base
   belongs_to :profile
 
   validates :title, presence: true, uniqueness: true, length: {minimum: 3}
-  validates :description, presence: true, length: {maxmum: 500}
-  validates :logo
-  validates :weblink
+  validates :description, presence: true, length: {maximum: 500}
+  # validates :logo
+  # validates :weblink
   validates :sourcecode, presence: true
 
 end
