2ccfe8392a76de483cf1bc70eef96c0af993063a
WindDrifter <ryanjcua@msn.com>
2015-09-18 18:50:29 -0700

    create model and controller for skill

---
+++ db/migrate/20150919014815_create_skills.rb
@@ -0,0 +1,11 @@
+class CreateSkills < ActiveRecord::Migration
+  def change
+    create_table :skills do |t|
+      t.string :name
+      t.integer :level
+      t.references :profile, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
