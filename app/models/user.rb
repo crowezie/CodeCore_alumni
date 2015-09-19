fe2f5887a26dc2c54117686a6db4c6c6db3df92d
Brett Crowe <crowe706@gmail.com>
2015-09-18 20:48:39 -0700

    navbar and some layout stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true,
   format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
-
+  def full_name
+    "#{first_name} #{last_name}".strip
+  end
 end
